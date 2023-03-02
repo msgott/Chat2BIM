3d651589c5e7b293357c836965baa1292e785c39
Maik Gottfried <maikgottfried@live.de>
2023-03-02 08:33:07 +0100

    Projektdateien hinzufügen.

---
+++ Chat2BIM/ChatbotHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Chat2BIM
+{
+    public class ChatbotHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
---
+++ Chat2BIM/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(Chat2BIM.Startup))]
+
+namespace Chat2BIM
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Weitere Informationen zum Konfigurieren Ihrer Anwendung finden Sie unter https://go.microsoft.com/fwlink/?LinkID=316888.
+            app.MapSignalR();
+        }
+    }
+}
